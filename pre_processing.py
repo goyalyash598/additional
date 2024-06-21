75c99b00423a6b3b0ffd9f45f947d711f795487c
goyalyash598 <125116496+goyalyash598@users.noreply.github.com>
2024-06-21 17:17:27 +0530

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -11,6 +11,7 @@ from openai import OpenAI
 import requests
 
 
+
 def upload_to_gemini(path, mime_type=None):
     file = genai.upload_file(path, mime_type=mime_type)
     return file
@@ -26,26 +27,51 @@ def extract_text_and_images(pdf_path):
         text = page.get_text()
         for img_index, img in enumerate(page.get_images(full=True)):
             xref = img[0]
-            base_image = doc.extract_image(xref)
-            image_data = base_image["image"]
-            images.append(image_data)
+            smask = img[1]
+            if(smask == 0):
+                base_image = doc.extract_image(xref)
+                image_data = base_image["image"]
+                images.append((page_num, img_index, image_data))
            
         extracted_text += text.strip()
 
+    # os.makedirs("Images", exist_ok=True)
+
     image_merge = []
     #Convert Image to IOBytes for for later Converting to Base64
-    for image_bytes in images:
+    for page_num, img_index, image_bytes in images:
         try:
             image_stream = io.BytesIO(image_bytes)
             image = Image.open(image_stream)
             image_merge.append(image)
+            # image.save(os.path.join("Images", f"image_{page_num}_{img_index}.png"))
+
         except Exception as e:
             with open("Error logs(bytes).txt","w") as f:
                 f.write(str(e))
+    # print(len(image_merge))
     return extracted_text,image_merge
 
 def preprocess_images(image_merge,model,m):
-
+    genai.configure(api_key=st.secrets["api_keys"]["genai_api_key"])
+    generation_config = {
+            "temperature": 1,
+            "top_p": 0.95,
+            "top_k": 64,
+            "max_output_tokens": 8192,
+            "response_mime_type": "text/plain",
+        }
+    safety_settings = [
+            {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+        ]
+    modelImage = genai.GenerativeModel(
+            model_name="gemini-1.5-flash-latest", 
+            safety_settings=safety_settings,
+            generation_config=generation_config,
+        )
     image_descriptions = []
     
     # step size determines the stacking of images
@@ -70,43 +96,16 @@ def preprocess_images(image_merge,model,m):
                 f.write(base64.b64decode(image_base64))
                 image_file = upload_to_gemini(image_path)
 
-            if m == "Gemini":
+            
                     
-                chat_session = model.start_chat(
+            chat_session = modelImage.start_chat(
                     history=[
                             {"role": "user", "parts": ["from my local image path, summarize the given image", image_file]},
                             {"role": "model", "parts": ["Your description of the image goes here."]},
                         ]
                     )
-                response = chat_session.send_message("Describe the image in more detail.").text
-            elif m == "Claude":
-                    message = model.messages.create(
-                    model="claude-3-sonnet-20240229",
-                    max_tokens=1024,
-                    messages=[
-                            {
-                                "role": "user",
-                                "content": [
-                                    {
-                                        "type": "image",
-                                        "source": {
-                                            "type": "base64",
-                                            "media_type": "image/jpeg",
-                                            "data": image_base64,
-                                        },
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": "Describe the image and learn the concept in it"
-                                    }
-                                ],
-                            }
-                        ],
-                    )
-                    response = message.content[0].text
-            else :
-                    response = gpt_description(image_base64)
-                    
+            response = chat_session.send_message("Describe the image in more detail.").text
+         
             image_descriptions.append(response)
 
             os.remove(image_path)
@@ -152,7 +151,7 @@ def combine_images(images, mode='vertical'):
             y_offset = (idx // cols) * max_height
             combined_image.paste(img, (x_offset, y_offset))
 
-    print(type(combined_image))
+    # print(type(combined_image))
     return combined_image
 
 def handle_pdf_file(pdf_file,model,m):
@@ -200,7 +199,7 @@ def gpt_description(image_base64):
     }
 
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-    print(response.json()["choices"][0]["message"]["content"])
+    # print(response.json()["choices"][0]["message"]["content"])
 
     return response.json()["choices"][0]["message"]["content"]
 
@@ -239,24 +238,25 @@ def model_selection(m):
     
 def generate_questions(model,m, combined_text, prompt, question_type,question_level, bloom,language, num_questions=10):
     if question_type == "Descriptive":
-        user_prompt = f"{prompt}\n\nBased on the following text, generate {num_questions} detailed descriptive questions of {question_level} Difficulty Level with correct answer only, Also ensure the the questions are {bloom} based as per Bloom's Taxonomy:\n\n{combined_text}.The questions should be in this {language}.\n The format of the output should match the following Regex- \*\*Question \d+:\*\* (.*?)\n\*\*Answer:\*\*\s(.*?)(?:\n\n|\n$)"
+        user_prompt = fr"{prompt}\n\nBased on the following text, generate {num_questions} detailed descriptive questions of {question_level} Difficulty Level with correct answer only, Also ensure the the questions are {bloom} based as per Bloom's Taxonomy:\n\n{combined_text}.The questions should be in this {language}.\n The format of the output should match the following Regex- \*\*Question \d+:\*\* (.*?)\n\*\*Answer:\*\*\s(.*?)(?:\n\n|\n$) \n For mathematical equations only keep them in format example $$f(x) = x^2 + 2x + 1$$"
     elif question_type == "MCQ":
-        user_prompt = f"""{prompt}\n\nBased on the following text, generate {num_questions} Multiple Choice Question(MCQs) and four options each and Answer of {question_level} Difficulty Level, Also ensure the the questions are {bloom} based as per Bloom's Taxonomy:\n\n{combined_text}.The questions should be in this {language}.\n The format of the output should match the following Regex- \*\*Question \d+:\*\* (.*?)\n\*\*Options:\*\*\n'
+        user_prompt = fr"""{prompt}\n\nBased on the following text, generate {num_questions} Multiple Choice Question(MCQs) and four options each and Answer of {question_level} Difficulty Level, Also ensure the the questions are {bloom} based as per Bloom's Taxonomy:\n\n{combined_text}.The questions should be in this {language}.\n The format of the output should match the following Regex- \*\*Question \d+:\*\* (.*?)\n\*\*Options:\*\*\n'
                         r'a\) (.*?)\n'
                         r'b\) (.*?)\n'
                         r'c\) (.*?)\n'
                         r'd\) (.*?)\n'
-                        r'\*\*Answer:\*\* (.*?)\n"""
+                        r'\*\*Answer:\*\* (.*?)\n For mathematical equations only keep them in format example $$f(x) = x^2 + 2x + 1$$"""
     elif question_type == "Fill in the Blanks":
-        user_prompt = f"{prompt}\n\nBased on the following text,Strictly generate {num_questions} fill in the blank questions only with correct answer. DO not Generate Descriptive or One word Question.The Question Should have a missing word replaced by blank that is the answer and the Difficulty should {question_level},  Also ensure the the questions are {bloom} based as per Bloom's Taxonomy:\n\n{combined_text}.The questions should be in this {language}.\n\n The format of the output should match the following Regex- \*\*Question \d+:\*\* (.*?)\n\*\*Answer:\*\*\s(.*?)(?:\n\n|\n$)"
-
-    try:
+        user_prompt = fr"{prompt}\n\nBased on the following text,Strictly generate {num_questions} fill in the blank questions only with correct answer. DO not Generate Descriptive or One word Question.The Question Should have a missing word replaced by blank that is the answer and the Difficulty should {question_level},  Also ensure the the questions are {bloom} based as per Bloom's Taxonomy:\n\n{combined_text}.The questions should be in this {language}.\n\n The format of the output should match the following Regex- \*\*Question \d+:\*\* (.*?)\n\*\*Answer:\*\*\s(.*?)(?:\n\n|\n$) \n For mathematical equations only keep them in format example $$f(x) = x^2 + 2x + 1$$"
+    # print(user_prompt)
+    # try:
+    if(1):
 
         if m == "Gemini":
             chat_session = model.start_chat(history=[{"role": "user", "parts": [user_prompt]}])
-            response = chat_session.send_message(f"Please provide {num_questions} questions as requested, without any additional context.")
-
-            print(type(response.text))
+            # response = chat_session.send_message(f"Please provide {num_questions} questions as requested, without any additional context.")
+            response = chat_session.send_message(f"Please do as Asked above.")
+            # print(type(response.text))
             return response.text
 
         elif m == "Claude":
@@ -282,6 +282,7 @@ def generate_questions(model,m, combined_text, prompt, question_type,question_le
             )
             return response.choices[0].message.content
 
-    except Exception as e:
-        st.error(f"Error generating questions: {e}")
-        return []
+    # except Exception as e:
+    #     st.error(f"Error generating questions: {e}")
+    #     return []
+
